619cd0b9fee56a7ff6b504be7fe8990d84cd7634
RedScorp0031 <42650171+RedScorp0031@users.noreply.github.com>
2026-04-23 22:25:33 +0200

    Almost nothing

---
+++ app/src/main/java/com/example/app/service/SecUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.app.service;
+
+import com.example.app.data.User;
+import com.example.app.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class SecUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public SecUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Nie znaleziono użytkownika: " + username));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority(user.getRole()))
+        );
+    }
+}
